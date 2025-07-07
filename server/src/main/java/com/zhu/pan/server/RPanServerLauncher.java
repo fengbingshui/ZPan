051564896664e29b383a4b0a06ee3c3d2ac9c808
zhutao <1308135918@qq.com>
2025-07-08 00:11:36 +0800

    1、初始化项目 - 代码生成

--- server/src/main/java/com/zhu/pan/server/RPanServerLauncher.java
+++ server/src/main/java/com/zhu/pan/server/RPanServerLauncher.java
@@ -3,6 +3,7 @@ package com.zhu.pan.server;
 import com.zhu.pan.core.constant.RPanConstants;
 import com.zhu.pan.core.response.R;
 import io.swagger.annotations.Api;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -11,13 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @SpringBootApplication(scanBasePackages = RPanConstants.BASE_COMPONENT_SCAN_PATH)
 @ServletComponentScan(basePackages = RPanConstants.BASE_COMPONENT_SCAN_PATH)
 @RestController()
 @Api("接口测试类")
 @Validated
+@MapperScan(basePackages = RPanConstants.BASE_COMPONENT_SCAN_PATH + ".server.modules.**.mapper")
 public class RPanServerLauncher {
     public static void main(String[] args) {
         SpringApplication.run(RPanServerLauncher.class,args);
@@ -26,6 +27,6 @@ public class RPanServerLauncher {
     @GetMapping( "hello")
     public R<String> hello(@NotBlank(message = "参数不为空") String name) {
         System.out.println(Thread.currentThread().getContextClassLoader());
-        return R.success("Hello " + name + "你好1");
+        return R.success("Hello " + name + "你好");
     }
 }
