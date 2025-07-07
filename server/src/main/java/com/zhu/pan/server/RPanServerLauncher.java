fe238f37b6433f467f41f9ab9807bea48aa448b6
zhutao <1308135918@qq.com>
2025-07-07 21:58:32 +0800

    1、初始化项目 - 全局异常

--- server/src/main/java/com/zhu/pan/server/RPanServerLauncher.java
+++ server/src/main/java/com/zhu/pan/server/RPanServerLauncher.java
@@ -6,20 +6,26 @@ import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @SpringBootApplication(scanBasePackages = RPanConstants.BASE_COMPONENT_SCAN_PATH)
 @ServletComponentScan(basePackages = RPanConstants.BASE_COMPONENT_SCAN_PATH)
 @RestController()
 @Api("接口测试类")
+@Validated
 public class RPanServerLauncher {
     public static void main(String[] args) {
         SpringApplication.run(RPanServerLauncher.class,args);
     }
 
-    @GetMapping("hello")
-    public R<String> hello(String name) {
-        return R.success("Hello " + name);
+    @GetMapping( "hello")
+    public R<String> hello(@NotBlank(message = "参数不为空") String name) {
+        System.out.println(Thread.currentThread().getContextClassLoader());
+        return R.success("Hello " + name + "你好1");
     }
 }
